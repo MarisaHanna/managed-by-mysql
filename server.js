391b55017d62780d3ffe451bac83864c3bed060d
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-04-23 12:01:53 -0700

    adding directory

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const sequelize = require('./config/connection');
 
 
@@ -11,3 +12,39 @@ app.use(express.urlencoded({extended: true}));
 sequelize.sync({force: true}).then (() => {
     app.listen(PORT, () => console.log (`Now listening on port ${PORT}`))
 });
+
+
+const start = () => {
+    inquirer.prompt (
+
+        {
+         type: 'list',
+         message: 'Hello! Please choose a directory below.',
+         choices: ['Add employee', 'View employee', 'Update employee', 'Delete employee', 'Exit'],
+         name: 'directory'  
+        }
+    ).then((data) => {
+       
+        switch (data.directory){
+
+            case 'Add employee':
+                addEmployee();
+                break;
+
+            case 'View employee':
+                viewEmployee();
+                break;
+                
+            case 'Update employee':
+                updateEmployee();
+                break;
+
+            case 'Exit':
+                console.log('See you next time! Goodbye!');
+                  
+
+        }
+    });
+}
+ exports.start = start;
+start();
